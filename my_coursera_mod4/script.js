3efde66e8f4856adde18b38476ee22fe165d1d5c
Gokul V n <gvnmb111@gmail.com>
2019-03-21 07:37:59 +0530

    Add files via upload

---
+++ my_coursera_mod4/script.js
@@ -0,0 +1,19 @@
+
+//IIFE
+(function(){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  //loop through all the names
+  for (var indx in names) {
+    //select lowercase of firt letter from name array
+    var firstLetter = names[indx].charAt(0).toLowerCase(); 
+
+    //call byeSpeaker for letters that begin with "j" else call helloSpeaker
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[indx]);
+    } else {
+      helloSpeaker.speak(names[indx]);
+    }
+  }
+
+})();
